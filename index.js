b3a46fe7053b543c9864508577216624c6a3fbd0
Mayankmer <48138626+Mayankmer@users.noreply.github.com>
2023-07-30 20:26:53 +0530

    removed bug, funciton getting array out of bound

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const choices = ["Rock", "Paper", "Scissors"]
 
 function getComputerChoice(){
-    let randomNum = Math.floor(Math.random()*3 + 1);
+    let randomNum = Math.floor(Math.random()*3);
     return choices[randomNum];
 }
+console.log(getComputerChoice());
