fb4f6c3f05d8e71da333a1f9161a8653f4b5cce6
Mayankmer <48138626+Mayankmer@users.noreply.github.com>
2023-07-30 22:23:36 +0530

    added a function to decide the winner, and function to loop that function

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const choices = ["Rock", "Paper", "Scissors"]
+const choices = ["rock", "paper", "scissors"]
 
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random()*3);
     return choices[randomNum];
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    if((playerSelection.toLowerCase() === "rock" && computerSelection === "paper") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors")||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock")){
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if(playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `Draw! You both chose ${playerSelection}`;
+    }
+}
+
+function game(){
+    for(let i=0; i<5; i++){
+        let playerSelection = prompt("Enter your choise");
+        const computerSelection = getComputerChoice();
+        alert(playRound(playerSelection, computerSelection));
+    }
+}
+game();
